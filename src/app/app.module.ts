ffea99ea6463f85c8330ddfeef48dd03bddcd015
AndriiChemer <ichemer@icloud.com>
2018-01-17 18:59:49 +0200

    update - app.module, add some components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,27 @@ import { NgModule } from '@angular/core';
 
 
 import { AppComponent } from './app.component';
+import { AuthComponent } from './auth/auth.component';
+import { LoginComponent } from './auth/login/login.component';
+import { RegistrationComponent } from './auth/registration/registration.component';
+import { SystemComponent } from './system/system.component';
+import { ShoppingCartComponent } from './system/shopping-cart/shopping-cart.component';
+import { HomeComponent } from './system/home/home.component';
+import { ProductsComponent } from './system/products/products.component';
+import { NavbarComponent } from './system/shared/components/navbar/navbar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    AuthComponent,
+    LoginComponent,
+    RegistrationComponent,
+    SystemComponent,
+    ShoppingCartComponent,
+    HomeComponent,
+    ProductsComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule
