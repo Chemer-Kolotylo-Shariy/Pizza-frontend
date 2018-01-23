0dffbaa8d3c113fe23a63198d82c7d52295774d9
AndriiChemer <ichemer@icloud.com>
2018-01-23 14:26:02 +0200

    update - navbar.component

--- src/app/system/shared/components/navbar/navbar.component.ts
+++ src/app/system/shared/components/navbar/navbar.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import {ShoppingCartServece} from "../../services/shopping-cart.servece";
+import {Cart} from "../../models/cart.model";
+import {Product} from "../../models/product.model";
 
 @Component({
   selector: 'app-navbar',
@@ -8,10 +11,22 @@ import { Component, OnInit } from '@angular/core';
 export class NavbarComponent implements OnInit {
 
   logo = '../../../../../assets/logo/logo.jpg';
+  photoPathStart = '../../../assets/pizza/';
+  photoPathEnd = '.png';
+  itemsCart: any = [];
 
-  constructor() { }
+  constructor(private cart:ShoppingCartServece) { }
 
   ngOnInit() {
+  }
+
+  loadCart(){
+    this.itemsCart = this.cart.loadCart();
+  }
+
+  remove(p: Product){
+    this.itemsCart = [];
+    this.itemsCart = this.cart.remove(p);
   }
 
 }
