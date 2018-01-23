84f2cc1ed8e24099b422f1399827eac9171e0a67
AndriiChemer <ichemer@icloud.com>
2018-01-23 15:25:44 +0200

    add - user.service and auth.service

---
+++ src/app/shared/services/user.service.ts
@@ -0,0 +1,21 @@
+import {BaseApi} from "../core/base-api";
+import {Http} from "@angular/http";
+import {Injectable} from "@angular/core";
+
+import {User} from "../models/user.model";
+
+@Injectable()
+export class UserService extends BaseApi{
+
+  user: User;
+
+  constructor(http: Http){
+    super(http);
+  }
+
+  getUserByPhone(phone: number){
+    return this.user = new User('Andrii', 'Chemer', 'ichemer@icloud.com', 537778445, '31lanafe', 'ADMIN', 1);
+  }
+
+  createUser(){}
+}
